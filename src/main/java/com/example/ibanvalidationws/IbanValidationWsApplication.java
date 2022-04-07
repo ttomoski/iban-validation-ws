60907fdff2bff164e7b1c71928435835af3ca487
Tomi Tomoski <Tomi.Tomoski@netcetera.com>
2022-04-07 10:48:04 +0200

    Initial commit

---
+++ src/main/java/com/example/ibanvalidationws/IbanValidationWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibanvalidationws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanValidationWsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IbanValidationWsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/ibanvalidationws/IbanValidationWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibanvalidationws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbanValidationWsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
