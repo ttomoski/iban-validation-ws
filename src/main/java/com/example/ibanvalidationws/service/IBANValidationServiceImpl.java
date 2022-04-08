2a100a064d4693b92c87957b1ad40dd9ebba0580
Magdalena Atanasovska <magdalena.atanasovska@gmail.com>
2022-04-08 10:21:20 +0200

    implemented business logic

--- src/main/java/com/example/ibanvalidationws/service/IBANValidationServiceImpl.java
+++ src/main/java/com/example/ibanvalidationws/service/IBANValidationServiceImpl.java
@@ -1,11 +1,79 @@
 package com.example.ibanvalidationws.service;
 
+import com.example.ibanvalidationws.model.IBANCountryCode;
+import com.example.ibanvalidationws.repository.IBANCountryCodeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+
+/**
+ * Implementation of {@link IBANValidationService}.
+ */
+@RequiredArgsConstructor
+@Service
+@Slf4j
 public class IBANValidationServiceImpl implements IBANValidationService {
+
+  private final IBANCountryCodeRepository ibanCountryCodeRepository;
 
   @Override
   public boolean validateIBAN(String iban) {
+    String formattedIban = iban.replaceAll(" ", "");
+    String code = formattedIban.substring(0, 2);
 
-    return false;
+    IBANCountryCode ibanCountryCode = ibanCountryCodeRepository.findByCode(code);
+
+    if (ibanCountryCode == null) {
+      log.error("Invalid or unsupported country code [{}]", code);
+      return false;
+    }
+
+    if (formattedIban.length() != ibanCountryCode.getLength()) {
+      log.error("Invalid IBAN length");
+      return false;
+    }
+
+    String rearrangedIBAN = rearrange(formattedIban);
+    String numericIBAN = convertToNumeric(rearrangedIBAN);
+
+    if (mod97(numericIBAN) != 1) {
+      log.error("Invalid IBAN");
+      return false;
+    }
+
+    log.info("IBAN validated successfully");
+    return true;
+  }
+
+  private String rearrange(String formattedIBAN) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append(formattedIBAN.substring(4));
+    stringBuilder.append(formattedIBAN, 0, 4);
+
+    return stringBuilder.toString();
+  }
+
+  private String convertToNumeric(String rearrangedIBAN) {
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int i = 0; i < rearrangedIBAN.length(); i++) {
+      char element = rearrangedIBAN.charAt(i);
+
+      if (Character.isLetter(element)) {
+        int number = element - 55;
+        stringBuilder.append(number);
+      } else {
+        stringBuilder.append(element);
+      }
+    }
+
+    return stringBuilder.toString();
+  }
+
+  private int mod97(String numericIban) {
+    return new BigInteger(numericIban).divideAndRemainder(BigInteger.valueOf(97))[1].intValue();
   }
 
 }
--- src/main/java/com/example/ibanvalidationws/web/IBANValidationController.java
+++ src/main/java/com/example/ibanvalidationws/web/IBANValidationController.java
@@ -1,19 +1,28 @@
-package com.example.ibanvalidationws.controller;
+package com.example.ibanvalidationws.web;
 
-import com.example.ibanvalidationws.model.IBANRequest;
 import com.example.ibanvalidationws.service.IBANValidationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for IBAN validation.
+ */
 @RestController
 @RequiredArgsConstructor
 public class IBANValidationController {
 
-  private IBANValidationService service;
+  private final IBANValidationService service;
 
+  /**
+   * Endpoint for validating an IBAN number.
+   *
+   * @param ibanRequest the IBAN
+   * @return whether the IBAN is valid
+   */
   @PostMapping("/validate")
-  public boolean validateIBAN(IBANRequest ibanRequest) {
+  public boolean validateIBAN(@RequestBody IBANRequest ibanRequest) {
     return service.validateIBAN(ibanRequest.getIban());
   }
 
